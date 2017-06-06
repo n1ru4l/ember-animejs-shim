f5f50336a7cab44a8a2a9ad43d6d930f186bcf0b
Laurin Quast <laurinquast@googlemail.com>
2017-06-06 08:34:09 +0200

    Linting setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,5 +9,15 @@ module.exports = {
     browser: true
   },
   rules: {
+    'quotes': [`error`, `backtick`, {
+      'avoidEscape': true
+    }],
+    'semi': [`error`, `never`],
+    'prefer-const': [`error`],
+    'comma-dangle': [`error`, `always-multiline`],
+    'object-curly-spacing': [`error`, `always`],
+    'array-bracket-spacing': [`error`, `always`, {
+      'objectsInArrays': false,
+    }],
   }
 };
